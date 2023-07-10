1c1703d2bc9af0878f46fe582d945e4dcd0fca27
Marco Testa <marco.testa@epublic.it>
2023-07-10 17:34:15 +0200

    EP: Palette colori

--- rollup.config.js
+++ rollup.config.js
@@ -142,17 +142,138 @@ export default [
     ],
   },
   // Entry for Comuni
+  /*{
+    input: 'src/scss/bi-verde-standard.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bootstrap-italia-comuni.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },*/
+
   {
-    input: 'src/scss/bootstrap-italia-comuni.scss',
-    output: {
-      dir: 'dist',
-    },
-    plugins: [
-      scss({
-        output: 'dist/css/bootstrap-italia-comuni.min.css',
-        outputStyle: 'compressed',
-        watch: 'src/scss',
-      }),
-    ],
+    input: 'src/scss/bi-blu-000066.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-000066.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-blu-00007A.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-00007A.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-blu-00238F.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-00238F.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-blu-00337A.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-00337A.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-blu-00517A.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-00517A.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-blu-005266.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-005266.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-blu-29007A.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-blu-29007A.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+
+  {
+    input: 'src/scss/bi-marrone-5C3D00.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-marrone-5C3D00.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-marrone-5C5C00.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-marrone-5C5C00.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+
+  {
+    input: 'src/scss/bi-rosso-660000.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-rosso-660000.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-rosso-660033.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-rosso-660033.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-rosso-662200.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-rosso-662200.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-rosso-7A0000.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-rosso-7A0000.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-rosso-992600.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-rosso-992600.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+
+  {
+    input: 'src/scss/bi-verde-005205.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-005205.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-005C00.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-005C00.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-005C1F.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-005C1F.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-005C3D.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-005C3D.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-005C5C.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-005C5C.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-135200.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-135200.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-2C5200.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-2C5200.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-verde-4A5C00.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-verde-4A5C00.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-viola-52007A.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-viola-52007A.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-viola-5C005C.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-viola-5C005C.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
+  },
+  {
+    input: 'src/scss/bi-viola-7A0052.scss',
+    output: { dir: 'dist' },
+    plugins: [scss({ output: 'dist/css/bi-viola-7A0052.min.css', outputStyle: 'compressed', watch: 'src/scss' })],
   },
 ]
